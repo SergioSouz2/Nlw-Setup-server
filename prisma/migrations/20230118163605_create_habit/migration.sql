9e3b04a04b6056871777ba9a097b5de8edb60709
SergioSouz2 <sergio.tbl0123@gmail.com>
2023-01-29 04:58:23 -0300

    subindo projeto Server da Nlw-Setup

---
+++ prisma/migrations/20230118163605_create_habit/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "habits" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Day" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "date" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "DayHabit" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "day_id" TEXT NOT NULL,
+    "habit_id" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Day_date_key" ON "Day"("date");
